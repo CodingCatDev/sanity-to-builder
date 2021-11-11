3054d5a4c1db133c86712849000f377e8c1d346b
Alex Patterson <alex@ajonp.com>
2021-11-11 18:17:07 -0500

    example works

--- src/index.ts
+++ src/index.ts
@@ -8,71 +8,115 @@ export const getAllTypes = async (list: string[], include = true) => {
   for (const [i, v] of list.entries()) {
     match += `_type match "${v}*" ${i != list.length - 1 ? ' || ' : ''}`;
   }
-  return [...new Set(await sanity.fetch(groq`*[${include ? '' : '!('}${match}${include ? '' : ')'}]._type`))];
+  const models = await sanity.fetch(
+    groq`*[${include ? '' : '!('}${match}${include ? '' : ')'}]._type`
+  );
+  if (!models || Object.keys(models).length === 0) {
+    return [];
+  }
+  return [...new Set(models)] as string[];
 };
 
-export const createModels = async () => {
-    // Get all types from Sanity
-    const unique = await getAllTypes(
-        ['system', 'kv', 'sanity', 'workflow', 'pluginSecrets'],
-        false
-    );
-    console.log('Creating models', JSON.stringify(unique));
-    if (!unique || Object.keys(unique).length === 0) {
-        return;
-    }
+export const getSingleDocument = async (type: string) => {
+  return await sanity.fetch(groq`*[_type == '${type}'][0]`);
+};
 
-    let chain = [];
-    for (const u of unique) {
-        chain.push(builder.chain.mutation.addModel({
-            body: {
-                defaultQuery: [],
-                kind: 'data',
-                showTargeting: true,
-                allowHeatmap: true,
-                id: u,
-                showMetrics: true,
-                publicReadable: true,
-                name: u,
-                useQueryParamTargetingClientSide: false,
-                fields: [
-                    {
-                        type: 'text',
-                        '@type': '@builder.io/core:Field',
-                        required: true,
-                        hideFromFieldsEditor: true,
-                        name: 'test',
-                        showTemplatePicker: true,
-                    },
-                ],
-                helperText: `This model is ${u}`,
-                allowBuiltInComponents: true,
-                bigData: false,
-                strictPrivateWrite: false,
-                requiredTargets: [],
-                schema: {},
-                examplePageUrl: 'https://my.site.com/preview',
-                webhooks: [],
-                apiGenerated: true,
-                showScheduling: true,
-                showAbTests: true,
-                pathPrefix: '/',
-                componentsOnlyMode: false,
-            },
-        }).execute({ id: true })
-        );
+export const isObject = (obj: any, k: any) => {
+  return typeof obj[k] == 'object' && obj[k] !== null;
+};
+
+const getFields = (fieldsObject: object) => {
+  const recursiveFields = (inbound: { [key: string]: any }) => {
+    const innerFields: any[] = [];
+    for (var k in inbound) {
+      innerFields.push({
+        simpleTextOnly: false,
+        disallowRemove: false,
+        showIf: '',
+        copyOnAdd: true,
+        name: k,
+        mandatory: false,
+        showTemplatePicker: true,
+        hidden: false,
+        subFields: isObject(inbound, k) ? recursiveFields(inbound[k]) : [],
+        type: isObject(inbound, k) ? 'object' : 'text',
+        helperText: '',
+        hideFromFieldsEditor: false,
+        noPhotoPicker: false,
+        autoFocus: false,
+        hideFromUI: false,
+        broadcast: false,
+        bubble: false,
+        onChange: '',
+        permissionsRequiredToEdit: '',
+        advanced: false,
+        model: '',
+        required: false,
+        '@type': '@builder.io/core:Field',
+      });
     }
-    await Promise.all(chain);
-}
+    return innerFields;
+  };
+  return recursiveFields(fieldsObject);
+};
+
+export const createModels = async (models: string[]) => {
+  console.log('Creating models', JSON.stringify(models));
+
+  let chain = [];
+  for (const m of models) {
+    //Get Each types fields
+    const modelRef = await getSingleDocument(m);
+    const fields = getFields(modelRef);
+    console.log(JSON.stringify(fields));
+    chain.push(
+      builder.chain.mutation
+        .addModel({
+          body: {
+            defaultQuery: [],
+            kind: 'data',
+            showTargeting: true,
+            allowHeatmap: true,
+            id: m,
+            showMetrics: true,
+            publicReadable: true,
+            name: m,
+            useQueryParamTargetingClientSide: false,
+            fields,
+            helperText: `This model is ${m}`,
+            allowBuiltInComponents: true,
+            bigData: false,
+            strictPrivateWrite: false,
+            requiredTargets: [],
+            schema: {},
+            examplePageUrl: 'https://my.site.com/preview',
+            webhooks: [],
+            apiGenerated: true,
+            showScheduling: true,
+            showAbTests: true,
+            pathPrefix: '/',
+            componentsOnlyMode: false,
+          },
+        })
+        .execute({ id: true })
+    );
+  }
+  await Promise.all(chain);
+};
 
 export const getModels = async () => {
-    const models = await builder.query({ models: { id: true, name: true, kind: true } });
-    return models.data?.models;
-}
-
+  const models = await builder.query({
+    models: { id: true, name: true, kind: true, fields: true },
+  });
+  return models.data?.models;
+};
 
 (async () => {
-    console.log('Before', await getModels())
-    await createModels()
-    console.log('After', await getModels())
+  // Get all types from Sanity
+  const models: string[] = await getAllTypes(
+    ['system', 'kv', 'sanity', 'workflow', 'pluginSecrets'],
+    false
+  );
+
+  await createModels(models);
 })();
